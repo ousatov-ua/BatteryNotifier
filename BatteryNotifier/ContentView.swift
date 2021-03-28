67433f6418c3195994b3997f73f934e68b1cd86d
Oleksii Usatov <usatovalexey81@gmail.com>
2021-03-28 15:20:27 +0300

    Changed layout

--- BatteryNotifier/ContentView.swift
+++ BatteryNotifier/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
     @State private var minValue: Double = 10
     @State private var maxValue : Double = 90
     
+    @State private var errorMessage:String = ""
+    
     
     init(batteryController: BatteryController){
         self.batteryController = batteryController;
@@ -21,27 +23,43 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Text("Specify lower and uppper limits:")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .padding(.vertical, 20)
+                .shadow(color: .blue, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+            
+            
             Text("Low battery notification: \(Int(minValue), specifier: "%d")")
+            
             Slider(value: $minValue, in: 10...80, step: 1)
                 .padding(.horizontal, 50.0).onAppear(perform: {
                     self.minValue = Double(self.batteryController.minLevel)
                 })
             Text("Full battery notification: \(Int(maxValue), specifier: "%d")")
-            Slider(value: $maxValue, in: 20 ... 90, step: 1)
+            
+            Slider(value: $maxValue, in: 20 ... 80, step: 1)
                 .padding(.horizontal, 50.0).onAppear(perform: {
                     self.maxValue = Double(self.batteryController.maxLevel);
                 })
             
-                Button(action: {
+            Button(action: {
+                if(minValue >= maxValue){
+                    errorMessage = "Low value cannot be greater than Full"
+                }else{
                     batteryController.minLevel = minValue
                     batteryController.maxLevel = maxValue
-                }){
-                    Text("Save")
-                        .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
-                        .cornerRadius(40)
-                        .overlay(RoundedRectangle(cornerRadius: 40).stroke(Color.blue, lineWidth: 2))
+                    errorMessage = ""
                 }
+            }){
+                Text("Save")
+                    .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
+                    .cornerRadius(40)
+                    .overlay(RoundedRectangle(cornerRadius: 40).stroke(Color.blue, lineWidth: 2))
             }
+            Text(errorMessage).padding(.vertical, 10)
+                .shadow(color: .red, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+            Spacer()
+        }.padding(.vertical, 50)
     }
 }
 
