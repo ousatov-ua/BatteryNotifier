99afa781692fda2e9180b37f06481a8cf18f5f0d
Oleksii Usatov <usatovalexey81@gmail.com>
2021-03-30 11:50:16 +0300

    Ios13 and less

--- BatteryNotifier/AppDelegate.swift
+++ BatteryNotifier/AppDelegate.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 
-
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDelegate {
+    
+    let batteryController:BatteryController;
+    let userNotificationCenter: UNUserNotificationCenter
+    let refreshInterval:Double = 5 * 60;
+    let backgroundRefreshInterval:Double = 15 * 60
+    let bgTaskId = "com.alus.product.BatteryMonitor.refresh"
 
+    override init() {
+        userNotificationCenter = UNUserNotificationCenter.current();
+        batteryController = BatteryController(userNotificationCenter: userNotificationCenter);
+        super.init()
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        batteryController.addMyselfAsObserver();
+        
+        userNotificationCenter.requestAuthorization(options: [.sound, .alert, .carPlay, .badge]) { (success, error) in
+            if let error = error {
+                print("Error: ", error)
+            }
+        }
+        self.userNotificationCenter.delegate = self
+        
+        UIApplication.shared.setMinimumBackgroundFetchInterval(UIApplication.backgroundFetchIntervalMinimum)
+        
+        // Additionally we add timer for level check
+        Timer.scheduledTimer(withTimeInterval: self.refreshInterval, repeats: true) { (t) in
+            self.batteryController.checkBatteryLevel(newValue: self.batteryController.getCurrentLevel())
+        }
         
         return true
     }
@@ -24,7 +51,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-
+        NotificationCenter.default.removeObserver(batteryController);
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner, .sound])
+    }
+    
+    func application(_ application: UIApplication,
+                     performFetchWithCompletionHandler completionHandler:
+                     @escaping (UIBackgroundFetchResult) -> Void) {
+        batteryController.checkBatteryLevel(newValue: batteryController.getCurrentLevel())
+        completionHandler(.noData)
+    }
+    
+  
+    
+    func applicationDidEnterBackground(_ application: UIApplication){
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        batteryController.addMyselfAsObserver()
     }
 
 }
--- BatteryNotifier/SceneDelegate.swift
+++ BatteryNotifier/SceneDelegate.swift
@@ -7,43 +7,20 @@
 
 import UIKit
 import SwiftUI
-import BackgroundTasks
 
-class AppRefreshOperation : Operation {
-    
-    let batteryController:BatteryController;
-    
-    init(batteryController: BatteryController){
-        self.batteryController = batteryController;
-    }
-    
-    override func main() {
-        batteryController.checkBatteryLevel(newValue: batteryController.getCurrentLevel())
-    }
-    
-}
+
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate, UNUserNotificationCenterDelegate {
     
     var window: UIWindow?
     
-    let batteryController:BatteryController;
-    let userNotificationCenter: UNUserNotificationCenter
-    let refreshInterval:Double = 5 * 60;
-    let backgroundRefreshInterval:Double = 15 * 60
-    let bgTaskId = "com.alus.product.BatteryMonitor.refresh"
     
     override init(){
-        userNotificationCenter = UNUserNotificationCenter.current();
-        batteryController = BatteryController(userNotificationCenter: userNotificationCenter);
-        super.init()
+      
     }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-
-        UIDevice.current.isBatteryMonitoringEnabled = true
-        batteryController.addMyselfAsObserver();
-       
+        let batteryController = (UIApplication.shared.delegate as! AppDelegate).batteryController
         let contentView = ContentView(batteryController: batteryController)
         
         if let windowScene = scene as? UIWindowScene {
@@ -52,31 +29,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, UNUserNotificationCente
             self.window = window
             window.makeKeyAndVisible()
         }
-        userNotificationCenter.requestAuthorization(options: [.sound, .alert, .carPlay, .badge]) { (success, error) in
-            if let error = error {
-                print("Error: ", error)
-            }
-        }
-        self.userNotificationCenter.delegate = self
-        
-        if #available(iOS 13, *) {
-            BGTaskScheduler.shared.register(
-                forTaskWithIdentifier: bgTaskId,
-                using: DispatchQueue.global()
-            ) { task in
-                self.handleAppRefresh(task)
-            }
-        }
-        
-        // Additionally we add timer for level check
-        Timer.scheduledTimer(withTimeInterval: self.refreshInterval, repeats: true) { (t) in
-            self.batteryController.checkBatteryLevel(newValue: self.batteryController.getCurrentLevel())
-        }
         
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
-        NotificationCenter.default.removeObserver(batteryController);
+        
     }
     
     func sceneDidBecomeActive(_ scene: UIScene) {
@@ -92,50 +49,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, UNUserNotificationCente
     }
     
     func sceneDidEnterBackground(_ scene: UIScene) {
-        UIDevice.current.isBatteryMonitoringEnabled = true
-        batteryController.addMyselfAsObserver()
-        
-        if #available(iOS 13, *) {
-            scheduleAppRefresh()
-        }
+        (UIApplication.shared.delegate as! AppDelegate).applicationDidEnterBackground(UIApplication.shared)
     }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        completionHandler()
-    }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.list, .banner, .sound])
-    }
-    
-    private func handleAppRefresh(_ task: BGTask) {
-        let queue = OperationQueue()
-        queue.maxConcurrentOperationCount = 1
-        let appRefreshOperation = AppRefreshOperation(batteryController: self.batteryController)
-        queue.addOperation(appRefreshOperation)
-        
-        task.expirationHandler = {
-            queue.cancelAllOperations()
-        }
-        
-        let lastOperation = queue.operations.last
-        lastOperation?.completionBlock = {
-            task.setTaskCompleted(success: !(lastOperation?.isCancelled ?? false))
-        }
-        
-        scheduleAppRefresh()
-    }
-    
-    @available(iOS 13.0, *)
-    private func scheduleAppRefresh() {
-        do {
-            let request = BGAppRefreshTaskRequest(identifier: bgTaskId)
-            request.earliestBeginDate = Date(timeIntervalSinceNow: backgroundRefreshInterval)
-            try BGTaskScheduler.shared.submit(request)
-        } catch {
-            print(error)
-        }
-    }
-    
 }
 
